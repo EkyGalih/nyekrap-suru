8d70dfffa946f6c6c21f85997b1425e4b603f8c5
EkyGalih <ekkygalih8@gmail.com>
2026-01-29 20:37:06 +0800

    ongoing series

--- app/api/drakorkita/series/ongoing/route.ts
+++ app/api/drakorkita/series/ongoing/route.ts
@@ -1,26 +1,29 @@
-import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-import { headers } from "@/src/lib/headers";
 import { scrapeOngoingSeries } from "@/src/lib/scrapers/drakorkita";
 import { withAuth } from "@/src/lib/withAuth";
+import { proxyFetchHTML } from "@/src/lib/proxyFetch";
 
 export const GET = withAuth(async (req: NextRequest) => {
     try {
         const page = req.nextUrl.searchParams.get("page") ?? "1";
 
-        const res = await axios.get<string>(
-            `${process.env.DRAKORKITA_URL}/all?page=${page}&status=returning%20series`,
-            { headers }
-        );
+        const url = `${process.env.DRAKORKITA_URL}/all?page=${page}&status=returning%20series`;
+        const html = proxyFetchHTML(url);
 
-        const data = await scrapeOngoingSeries(res);
+        const data = await scrapeOngoingSeries({ data: html } as any);
 
         return NextResponse.json({
             message: "success",
             page: parseInt(page),
             ...data,
-        });
+        },
+            {
+                headers: {
+                    "Cache-Control": "s-maxage=600, stale-while-revalidate=180",
+                },
+            }
+        );
     } catch (err: unknown) {
         return NextResponse.json(
             {
