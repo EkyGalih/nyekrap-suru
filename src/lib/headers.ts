9234ea090fbe3d1c5f86ea94bbf67911a4cb32c9
EkyGalih <ekkygalih8@gmail.com>
2026-01-29 16:02:06 +0800

    fix::headers

--- src/lib/headers.ts
+++ src/lib/headers.ts
@@ -1,4 +1,8 @@
 export const headers = {
   "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+  "Accept-Language": "id-ID,id;q=0.9,en;q=0.8",
+  Referer: process.env.DRAKORKITA_URL,
 };
