e5b09e3dbf0db4d2eb37fba42d74809ebb0af7c8
EkyGalih <ekkygalih8@gmail.com>
2026-02-13 15:22:06 +0800

    fix::anime detail

--- app/api/anime/detail/[slug]/route.ts
+++ app/api/anime/detail/[slug]/route.ts
@@ -1,8 +1,8 @@
+import { proxyFetchHTML } from '@/src/lib/proxyFetch';
 import { NextResponse } from "next/server"
 import { getOrScrape } from "@/src/lib/anime/getOrScrape"
 import { scrapeOtakudesuDetail } from "@/src/lib/scrapers/anime"
 import { withAuth } from "@/src/lib/withAuth"
-import { fetchHTMLAnime } from "@/src/lib/fetchHtmlAnime"
 
 export const runtime = "nodejs"
 
@@ -12,11 +12,11 @@ export const GET = withAuth(async (_req, context) => {
     const data = await getOrScrape({
         cacheKey: `anime:detail:${slug}`,
         endpoint: `/anime/${slug}/`,
-        ttl: 60 * 60 * 24,
+        ttl: 60 * 60 * 24 * 7,
         allowStaleOnError: true,
         scraper: async () => {
             const url = `${process.env.OTAKUDESU_URL}/anime/${slug}/`
-            const html = await fetchHTMLAnime(url)
+            const html = await proxyFetchHTML(url)
             return scrapeOtakudesuDetail(html)
         }
     })
